fa1134b1d26e22419321ec57a4a1163c52ea7618
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2022-05-07 20:55:30 -0600

    improve comments

--- Data.java
+++ Data.java
@@ -6,15 +6,23 @@ public class Data
 	private int[][] grid;
 
 	@SuppressWarnings("unused")
-	private Data()
-	{
-		/* prevent uninitalized instances */}
+	private Data() { /* prevent uninitalized instances */}
 
 	public Data(int rows, int cols)
 	{
 		grid = new int[rows][cols];
 	}
 
+	/**
+	 * <b>Purpose:</b> fills all elements of <code>grid</code> with randomly
+	 * generated values that adhere to the constraints described in part (a).
+	 * <p>
+	 * <b>Preconditions:</b>
+	 * <ul>
+	 * <li><code>grid</code> is not null
+	 * <li><code>grid</code> has at least one element
+	 * </ul>
+	 */
 	public void repopulate()
 	{
 		for (int r = 0; r < grid.length; r++)
@@ -27,9 +35,14 @@ public class Data
 	}
 
 	/**
-	 * Returns a usable random value that meats
+	 * <b>Purpose:</b> Returns a usable random value that meets the following conditions:
+	 * <ul>
+	 * <li>The value is between 1 and <code>MAX</code>
+	 * <li>The value is divisible by 10
+	 * <li>The value is not divisible by 100
+	 * </ul>
 	 * 
-	 * @return
+	 * @return - the random number
 	 */
 	public int getUsableRandomValue()
 	{
@@ -55,6 +68,12 @@ public class Data
 		return retval;
 	}
 
+	/**
+	 * <b>Purpose:</b> Counts the number of columns where the values in each row
+	 * of that column are equal to or greater than the row above it.
+	 * <p>
+	 * @return the number of columns with increasing rows
+	 */
 	public int countIncreasingCols()	
 	{
 		int n = 0;
@@ -67,7 +86,7 @@ public class Data
 				current = grid[r][c];
 				if(current < prev)
 				{
-					// not increasing, so skip to next column
+					// not increasing, so skip to the next column
 					// by short-circuiting the inner loop
 					isIncreasing = false;
 				}
