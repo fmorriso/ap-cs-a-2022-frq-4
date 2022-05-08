461e0d0d087a332c33a408d8bd72f331c9a5c7f5
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2022-05-07 20:44:40 -0600

    Part B works

--- Data.java
+++ Data.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 public class Data
 {
-	public static final int MAX = 4000;
-	private int[][] grid;	
-	
+	public static final int MAX = 2000;
+	private int[][] grid;
+
 	@SuppressWarnings("unused")
-	private Data() {/* prevent uninitalized instances */}
-	
+	private Data()
+	{
+		/* prevent uninitalized instances */}
+
 	public Data(int rows, int cols)
 	{
 		grid = new int[rows][cols];
@@ -15,26 +17,27 @@ public class Data
 
 	public void repopulate()
 	{
-		for(int r = 0; r < grid.length; r++)
+		for (int r = 0; r < grid.length; r++)
 		{
-			for(int c = 0; c < grid[r].length; c++)
+			for (int c = 0; c < grid[r].length; c++)
 			{
 				grid[r][c] = getUsableRandomValue();
 			}
-		}		
+		}
 	}
-	
+
 	/**
-	 * Returns a usable random value
+	 * Returns a usable random value that meats
+	 * 
 	 * @return
 	 */
 	public int getUsableRandomValue()
 	{
 		final int MIN = 1, RANGE = MAX - MIN + 1;
 		int val = 0;
-		while(val < MIN || val % 10 != 0 || val % 100 == 0 )
+		while (val < MIN || val % 10 != 0 || val % 100 == 0)
 		{
-			val = (int)(Math.random() * RANGE) + MIN;
+			val = (int) (Math.random() * RANGE) + MIN;
 		}
 		return val;
 	}
@@ -43,18 +46,38 @@ public class Data
 	public String toString()
 	{
 		String retval = "";
-		
-		 for (int[] row : grid)
+
+		for (int[] row : grid)
 		{
 			retval += Arrays.toString(row) + "\n";
 		}
-		 
+
 		return retval;
 	}
 
-	public int countIncreasingCols()
+	public int countIncreasingCols()	
 	{
 		int n = 0;
+		for(int c = 0; c < grid[0].length; c++)
+		{
+			int prev = 0, current;
+			boolean isIncreasing = true;
+			for(int r = 0; r < grid.length && isIncreasing; r++)
+			{
+				current = grid[r][c];
+				if(current < prev)
+				{
+					// not increasing, so skip to next column
+					// by short-circuiting the inner loop
+					isIncreasing = false;
+				}
+				else
+				{
+					prev = current;
+				}
+			}
+			if(isIncreasing) n++;
+		}
 		return n;
 	}
 }
--- Driver.java
+++ Driver.java
@@ -7,23 +7,38 @@ public class Driver
 		PartA();
 		PartB();
 	}
-
-	private static void PartB()
-	{
-		Data d = new Data(2, 3);
-		d.repopulate();
-		System.out.println("Grid:\n" + d);
-		int n = d.countIncreasingCols();
-		System.out.println("\nNumber of increasing columns = " + n);
-		
-	}
-
+	
 	private static void PartA()
 	{
+		System.out.println("\nPart A:");
 		Data d = new Data(3, 3);
 		d.repopulate();
 		System.out.println("Grid:\n" + d);
+	}
+
+	private static void PartB()
+	{
+		System.out.println("\nPart B:");
+		System.out.println("2 x 3 Grid:");
+		Data d = new Data(2, 3);
+		d.repopulate();
+		System.out.print(d);
+		int n = d.countIncreasingCols();
+		System.out.println("Number of increasing columns = " + n);
 		
+		System.out.println("\n2 x 4 Grid:");
+		d = new Data(2, 4);
+		d.repopulate();
+		System.out.print(d);
+		n = d.countIncreasingCols();
+		System.out.println("Number of increasing columns = " + n);	
+		
+		System.out.println("\n3 x 12 Grid:");
+		d = new Data(3, 12);
+		d.repopulate();
+		System.out.print(d);
+		n = d.countIncreasingCols();
+		System.out.println("Number of increasing columns = " + n);
 	}
 
 	/**
@@ -31,11 +46,11 @@ public class Driver
 	 */
 	private static void verifyRandomValueGenerator()
 	{
+		System.out.println("verify random value generator works:");
 		Data d = new Data(1, 1);
 		for (int i = 0; i < 10; i++)
 		{
-			System.out.println(d.getUsableRandomValue());
-			
+			System.out.println(d.getUsableRandomValue());			
 		}		
 	}
 
