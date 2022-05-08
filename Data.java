c1e9ab5f0e90fd689d9f5a1f688a095348906343
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2022-05-07 18:42:07 -0600

    Part A works

--- Data.java
+++ Data.java
@@ -1,7 +1,8 @@
+import java.util.Arrays;
 
 public class Data
 {
-	public static final int MAX = 100;
+	public static final int MAX = 1000;
 	private int[][] grid;	
 	
 	@SuppressWarnings("unused")
@@ -10,5 +11,44 @@ public class Data
 	public Data(int rows, int cols)
 	{
 		grid = new int[rows][cols];
+	}
+
+	public void repopulate()
+	{
+		for(int r = 0; r < grid.length; r++)
+		{
+			for(int c = 0; c < grid[r].length; c++)
+			{
+				grid[r][c] = getUsableRandomValue();
+			}
+		}		
+	}
+	
+	/**
+	 * Returns a usable random value
+	 * @return
+	 */
+	public int getUsableRandomValue()
+	{
+		final int MIN = 1, RANGE = MAX - MIN + 1;
+		int val = 0;
+		while(val < MIN || val % 10 != 0 || val % 100 == 0 )
+		{
+			val = (int)(Math.random() * RANGE) + MIN;
+		}
+		return val;
+	}
+
+	@Override
+	public String toString()
+	{
+		String retval = "grid: ";
+		
+		 for (int[] row : grid)
+		{
+			retval += Arrays.toString(row) + "\n";
+		}
+		 
+		return retval;
 	}
 }
--- Driver.java
+++ Driver.java
@@ -1,11 +1,33 @@
 
 public class Driver
 {
-
 	public static void main(String[] args)
 	{
-		Data d = new Data(3, 2);
+		verifyRandomValueGenerator();
+		PartA();
+		
+	}
 
+	private static void PartA()
+	{
+		Data d = new Data(2, 3);
+		d.repopulate();
+		System.out.println(d);
+		
+	}
+
+	/**
+	 * Pseudo-unit test of method getUsableRandomValue in the Data class.
+	 */
+	private static void verifyRandomValueGenerator()
+	{
+		Data d = new Data(1, 1);
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.println(d.getUsableRandomValue());
+			
+		}
+		
 	}
 
 }
